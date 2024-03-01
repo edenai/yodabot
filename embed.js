46cde8f178ad54a682a2416f780d8cc6f2b99362
Cedric-Font <font.cedric38@gmail.com>
2024-03-01 15:17:36 +0100

    minify v2

--- embed.js
+++ embed.js
@@ -3,7 +3,6 @@ window.addEventListener("load", () => {
   let ss = document.getElementsByTagName(l);let qp="";for(const t of ss){if(
   t.src.indexOf(n)>-1&&t.src.indexOf('?')>-1){qp = t.src.slice(t.src.indexOf('?'));break;}}
   let p=document.createElement(s);p.id=i;
-
   p.style="position:fixed;z-index:123456789;right:10px;bottom:0;height:96px;width:400px;min-height:96px;min-width:260px";
   let m=document.createElement(f);m.src=`${o}${qp}`;m.style="width: 100%; height: 100%; border: 0";p.appendChild(m);
   document.body.appendChild(p)})("div","edenai-message-iframe-container","iframe","https://edenai.github.io/yodabot/",'script','gh/edenai/yodabot');
@@ -11,17 +10,18 @@ window.addEventListener("load", () => {
   let cbc=document.getElementById(i);switch(ev.data){case h:cbc.style.height=v;cbc.style.width=w;
   break;case s:cbc.style.height=x;cbc.style.width=y}},false)})("message","https://edenai.github.io",
   "edenai-message-iframe-container","hide","show","96px","400px","760px","px");
-  const iframe=document.getElementById("edenai-message-iframe-container");
-  let w=window.innerWidth;const ref=document.getElementsByClassName("visible");
+  (function(y,o,d,a){const iframe=document.getElementById(y);
+  let w=window.innerWidth;const ref=document.getElementsByClassName(o);
   w<760&&ref?(console.log(ref?ref[0]:"no ref"),
-  iframe.style.width="90%",iframe.style.maxWidth="90%",iframe.classList.add("mobile"))
-  :(document.getElementById("edenai-message-iframe-container").style.width="400px",iframe.classList.contains("mobile")&&(iframe.classList.remove("mobile")
-  ,ref[0].style.marginTop="26%!important")),window.addEventListener("resize",
-  (function(e){const t=document.getElementById("edenai-message-iframe-container");
-  let i=window.innerWidth;const s=document.getElementsByClassName("visible");
-  i<760&&s?(t.style.width="90%",t.style.maxWidth="90%",t.classList.add("mobile"),
-  s[0].style.border="solid 1px #255fff",s[0].style.marginTop="41%!important"):(document.getElementById("edenai-message-iframe-container")
-  .style.width="400px",t.classList.contains("mobile")&&(t.classList.remove("mobile"),s[0].style.marginTop="26%!important"))}));
+  iframe.style.width="90%",iframe.style.maxWidth="90%",iframe.classList.add(d))
+  :(document.getElementById(y).style.width="400px",iframe.classList.contains(d)&&(iframe.classList.remove(d)
+  ,ref[0].style.marginTop="26%!important")),window.addEventListener(a,
+  (function(e){const t=document.getElementById(y);
+  let i=window.innerWidth;const s=document.getElementsByClassName(o);
+  i<760&&s?(t.style.width="90%",t.style.maxWidth="90%",t.classList.add(d),
+  s[0].style.border="solid 1px #255fff",s[0].style.marginTop="41%!important"):(document.getElementById(y)
+  .style.width="400px",t.classList.contains(d)&&(t.classList.remove(d),s[0].style.marginTop="26%!important"))}));
   });
+})("edenai-message-iframe-container","visible","mobile","resize");
 
 
